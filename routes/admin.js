11bc8d2973b3dac641b708a32fd569b5c4da324f
rlkilger <rlkilger@gmail.com>
2021-09-28 17:33:53 -0600

    week 3 initial

--- routes/admin.js
+++ routes/admin.js
@@ -2,41 +2,23 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title, price: req.body.price, desc: req.body.desc });
-  res.redirect('/');
-});
+router.post('/add-product', adminController.postAddProduct);
 
-router.post('/remove-product', (req, res, next) => {
-  const removeProduct = req.body.removeProduct;
-  products.forEach(prod => {
-    if (prod.title === removeProduct) {
-      products.splice(prod, 1);
-      console.log(`removed ${prod.title}`);
-      return prod;
-    }
-  });
-  res.redirect('/');
-});
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
+router.post('/edit-product', adminController.postEditProduct);
 
-exports.routes = router;
-exports.products = products;
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
